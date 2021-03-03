df373a4fe7483ea19c77d5c40d960bf5d2507298
mayoko <mayokoex@gmail.com>
2021-03-03 13:26:28 +0900

    add tests for Parser

--- src/Parser.rs
+++ src/Parser.rs
@@ -20,7 +20,7 @@ impl Parser {
 
     // move to the next line.
     // if parser reached to the last line or something goes wrong, 
-    // it returns false. Other wise, it returns true.
+    // it returns false. Otherwise, it returns true.
     fn advance(&mut self) -> bool {
         match self.line_iter.next() {
             None => return false,
@@ -59,4 +59,28 @@ impl Parser {
 mod tests {
     use super::*;
 
+    #[test]
+    fn should_work() {
+        // the file contains 3 lines
+        let mut parser = Parser::new("test/commands.txt");
+        assert_eq!(parser.advance(), true);
+        // @1000
+        assert!(matches!(parser.command_type(), CommandType::A));
+        assert_eq!(parser.symbol(), Some("1000"));
+
+        assert_eq!(parser.advance(), true);
+        // D=A
+        assert!(matches!(parser.command_type(), CommandType::C));
+        assert_eq!(parser.dest(), Some("D"));
+        assert_eq!(parser.comp(), Some("A"));
+        assert_eq!(parser.jump(), None);
+
+        assert_eq!(parser.advance(), true);
+        // (LABEL)
+        assert!(matches!(parser.command_type(), CommandType::L));
+        assert_eq!(parser.symbol(), Some("LABEL"));
+
+        assert_eq!(parser.advance(), false);
+        assert_eq!(parser.advance(), false);
+    }
 }
